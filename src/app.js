eba5920ca372aaee32e4fadd5feec5a870da25ad
DanAparicio22 <daparicio52@gmail.com>
2018-10-25 17:38:13 -0400

    Add port to deploy

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ var app = express();
 var port = 1234;
 var server = http.createServer(app);
 
-server.listen(port);
+server.listen(process.env.PORT || port);
 routes(app);
 
 server.on('listening', function () {
